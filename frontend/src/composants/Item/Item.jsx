31b6689bf6a5273326c1bfca309cebd1eaa63662
bachir423 <bachir.bouchoukh42@gmail.com>
2025-09-17 04:10:40 +0200

    ...

--- frontend/src/composants/Item/Item.jsx
+++ frontend/src/composants/Item/Item.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Item.css'
 const Item = (props) => {
   return (
     <div>
@@ -8,10 +8,10 @@ const Item = (props) => {
 
       <div className="item_prices">
         <div className="new_price">
-         <p>{props.new_price}</p>
+         <p>{props.new_price}€</p>
         </div>
         <div className="old_price">
-         <p>{props.old_price}</p>
+         <p>{props.old_price}€</p>
         </div>
       </div>
     </div>
