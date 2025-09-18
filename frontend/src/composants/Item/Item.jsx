d39bc9ecabf6de83fc87cdf0cbd1bd03df812a71
bachir423 <bachir.bouchoukh42@gmail.com>
2025-09-18 03:04:10 +0200

    ...

--- frontend/src/composants/Item/Item.jsx
+++ frontend/src/composants/Item/Item.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Item.css'
 const Item = (props) => {
   return (
-    <div>
+    <div className="item">
       <img src={props.image} alt="" />
       <p>{props.name}</p>
 
